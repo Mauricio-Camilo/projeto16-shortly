d48ba5941fb7d4b0e2e7a6fc24ff6c0dae398774
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-06-07 09:36:03 -0500

    add server

---
+++ index.js
@@ -0,0 +1,29 @@
+import express, {json} from "express";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+import db from "./database.js"
+
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.get("/teste", async (req, res) => {
+    console.log("Teste feito");
+    try {
+        const result = await db.query(`SELECT * FROM categories`);
+        res.send(result.rows);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500); // internal server error
+      }
+    // res.sendStatus(200);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, 
+    () => {console.log(chalk.bold.blue(`Servidor conectado na porta ${PORT}`))});
