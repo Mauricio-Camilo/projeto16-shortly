2f509cabcdb69a6dab8aaa1dac531d0083b16055
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-06-09 11:32:51 -0500

    feat: add delete urls/:id route

--- controllers/urlControllers.js
+++ controllers/urlControllers.js
@@ -15,7 +15,7 @@ export async function postShorten(req, res) {
         const {user} = res.locals;
 
         const shortenURL = nanoid(10);
-        
+
         await db.query(`INSERT INTO urls ("shortUrl", url, "userId")
         VALUES ($1, $2, $3)`, [shortenURL, req.body.url, user.rows[0].id]);
         res.send(shortenURL).status(200);
@@ -37,6 +37,30 @@ export async function getUrlId(req, res) {
             return res.send({ id, shortUrl, url });
         }
     }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function deleteUrlId(req, res) {
+
+    try {
+
+        const { id } = req.params; 
+        const {user} = res.locals;
+
+        const checkUrl = await db.query(`SELECT * FROM urls WHERE id=$1`, [id]);
+
+        if (checkUrl.rowCount === 0) return res.sendStatus(404);
+
+        if (user.rows[0].id !== checkUrl.rows[0].userId) return res.sendStatus(401);
+
+        await db.query(`DELETE FROM urls WHERE id=$1`, [checkUrl.rows[0].id])
+        return res.sendStatus(204);
+
+    }
+
     catch (error) {
         console.log(error);
         res.sendStatus(500);
--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import { postShorten, getUrlId } from "../controllers/urlControllers.js";
+import { postShorten, getUrlId, deleteUrlId } from "../controllers/urlControllers.js";
 import validateToken from "./../middlewares/authMiddlewares.js"
 
 const urlRouter = Router();
 
-urlRouter.post("/urls/shorten",validateToken, postShorten);
+urlRouter.post("/urls/shorten", validateToken, postShorten);
 urlRouter.get("/urls/:id", getUrlId);
+urlRouter.delete("/urls/:id", validateToken, deleteUrlId)
 
 
 export default urlRouter;
